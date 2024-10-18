d7f4f10131bfd891f2002eead28059d96f48107c
richik96 <drichik@gmail.com>
2024-10-18 11:43:25 +0530

    init

---
+++ src/main/java/com/scaler/user_service_oct24/UserServiceOct24Application.java
@@ -0,0 +1,13 @@
+package com.scaler.user_service_oct24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceOct24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceOct24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/user_service_oct24/UserServiceOct24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.user_service_oct24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceOct24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
